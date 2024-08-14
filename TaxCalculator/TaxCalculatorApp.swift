e8e633a17e806d46695262c8ba2737654acf3091
Haruki Tasaka <paparukisan@gmail.com>
2024-08-14 22:45:10 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by 田坂青輝 on 2024/08/14.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
